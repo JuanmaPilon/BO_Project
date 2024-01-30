ce5b9894e967e3601217025b28ec1eca84b62a07
Juanma <juanmartin.pilon@gmail.com>
2024-01-30 16:49:23 -0300

    Calling it for the day. Added personalized fonts, tried to make landing page similar as possible as the figma model. Also layout is working properly, tomorrow will continue with other pages. Still have to align login button.

--- frontend-vue/pages/index.vue
+++ frontend-vue/pages/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    <h1 class="font-extrabold text-5xl text-center">Welcome!</h1>
+    <header>
+      <div>
+        <h1 class="font-extrabold text-5xl text-center">Welcome!</h1>
+      </div>
+    </header>
+
+    <main class="container mx-auto my-8 flex items-center justify-content">
+      <div class="w-full max-w-md">
+        <form action="#">
+          <div class="mb-4">
+            <label for="#" class="inputText">User</label>
+            <input type="text" id="username" name="username" class="inputBox" placeholder="john@doe.com"/>
+          </div>
+          <div>
+            <label for="#" class="inputText">Password</label>
+            <input type="text" id="password" name="password" class="inputBox" placeholder="**********"/>
+          </div>
+          <button type="submit" class="btn">Login</button>
+        </form>
+      </div>
+    </main>
   </div>
 </template>
 
