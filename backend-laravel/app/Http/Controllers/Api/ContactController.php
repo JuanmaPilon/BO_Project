76294dc6b0121ba03e09da47ceb052b7af103309
Juanma <juanmartin.pilon@gmail.com>
2024-02-05 17:55:29 -0300

    Working endpoint and tested in postman, adding to DB correctly

--- backend-laravel/app/Http/Controllers/Api/ContactController.php
+++ backend-laravel/app/Http/Controllers/Api/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
@@ -22,5 +23,43 @@ class ContactController extends Controller
         'status' => 404,
         'message' => 'No contacts found'
     ],404);
+}
+
+public function store(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name'=> 'required|string|max:191',
+        'position'=> 'required|string|max:191',
+        'adress'=> 'required|string|max:191',
+        'email'=> 'required|email|max:191',
+        'number'=> 'required|string|max:191',
+    ]);
+
+    if($validator->fails()) {
+        return response()->json([[
+            'status'=> 422,
+            'errors' => $validator->messages()
+        ]],422);
+
+    } else {
+        $contact = Contact::create([
+            'name'=> $request->name,
+            'position'=> $request->position,
+            'adress'=> $request->adress,
+            'email'=> $request->email,
+            'number'=> $request->number
+        ]);
+
+        if($contact) {
+            return response()->json ([
+                'status'=> 200,
+                'message'=> 'Contact created successfully'
+            ]);
+        } else {
+            return response()->json ([
+                'status'=> 500,
+                'message'=> 'Somethig went wrong, try again'
+            ]);
+        }
+    }
 }
 }
--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('contact', [ContactController::class, 'index']);
+Route::post('contact', [ContactController::class, 'store']);
