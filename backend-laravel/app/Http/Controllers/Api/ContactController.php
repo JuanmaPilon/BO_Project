91c3fcedd6e8db670f10e270e8328963b458a9ae
Juanma <juanmartin.pilon@gmail.com>
2024-02-06 14:08:00 -0300

    added the rest of CRUD operation, now its working properly, tested with postman, now going to try to do the connection with frontend to fetch data

--- backend-laravel/app/Http/Controllers/Api/ContactController.php
+++ backend-laravel/app/Http/Controllers/Api/ContactController.php
@@ -62,4 +62,93 @@ public function store(Request $request) {
         }
     }
 }
+
+public function show($id) {
+    $contact = Contact::find($id);
+    if($contact) {
+        return response()->json ([
+            'status'=> 200,
+            'contact'=> $contact
+        ]);
+    } else {
+        return response()->json([
+            'status'=> 404,
+                'message'=> 'No contact found'
+                ], 404);
+    }
+}
+
+public function edit($id) {
+    $contact = Contact::find($id);
+    if($contact) {
+        return response()->json ([
+            'status'=> 200,
+            'contact'=> $contact
+        ]);
+    } else {
+        return response()->json([
+            'status'=> 404,
+                'message'=> 'No contact found'
+                ], 404);
+    }
+}
+
+public function update(Request $request, $id) {
+    $validator = Validator::make($request->all(), [
+        'name'=> 'required|string|max:191',
+        'position'=> 'required|string|max:191',
+        'adress'=> 'required|string|max:191',
+        'email'=> 'required|email|max:191',
+        'number'=> 'required|string|max:191',
+    ]);
+
+    if($validator->fails()) {
+        return response()->json([[
+            'status'=> 422,
+            'errors' => $validator->messages()
+        ]],422);
+
+    } else {
+        $contact = Contact::find($id);
+
+
+        if($contact) {
+            $contact->update([
+                'name'=> $request->name,
+                'position'=> $request->position,
+                'adress'=> $request->adress,
+                'email'=> $request->email,
+                'number'=> $request->number
+            ]);
+
+            return response()->json ([
+                'status'=> 200,
+                'message'=> 'Contact updated successfully'
+            ]);
+        } else {
+            return response()->json ([
+                'status'=> 500,
+                'message'=> 'Something went wrong, no contact found'
+            ]);
+        }
+    }
+}
+
+
+public function destroy($id) {
+    $contact = Contact::find($id);
+    if($contact) {
+        $contact->delete();
+        return response()->json([
+            'status' => 200,
+            'message'=> 'Contact was deleted successfully'
+            ],200);
+    }else {
+        return response()->json ([
+            'status'=> 500,
+            'message'=> 'Something went wrong, no contact found'
+        ]);
+    }
+}
+
 }
