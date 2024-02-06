451bc64f2e55b4d79fc0332e5759be7fff3c095e
Juanma <juanmartin.pilon@gmail.com>
2024-02-06 17:55:51 -0300

    Added a modal for the form so it would be nice to have a pop up, fill the form and submit it. Next is testing the connection from frontend with the backend using the api. Pushing the changes, will continue tomorrow

--- frontend-vue/components/contactModal.vue
+++ frontend-vue/components/contactModal.vue
@@ -3,7 +3,7 @@
       <div class="modal bg-white text-center p-8 rounded-lg">
         <h6 class="font-bold text-2xl mb-4">Saved!</h6> 
         <p class="text-base mb-4">ADDDINGGGG CONTACT</p>
-        <button class="btn-pinkBox">Submit??</button>
+        <button class="pinkBox">Submit??</button>
       </div>
       <div class="close absolute top-4 right-4 cursor-pointer" @click="$emit('close-modal')">
         <button class="btn-close w-10 h-10">X</button>
