68a983e051fcd3e018cc01e9fe01ecd2ceed61a1
Juanma <juanmartin.pilon@gmail.com>
2024-02-27 12:22:42 -0300

    added axios which i forgot to add to contactProfile.vue

--- frontend-vue/pages/contactProfile.vue
+++ frontend-vue/pages/contactProfile.vue
@@ -24,6 +24,7 @@
   import contactInfo from '#components';
   import { useAuthStore } from '#imports';
   import { useContactStore } from '#imports';
+  import axios from 'axios';
 
 export default {
   data() {
