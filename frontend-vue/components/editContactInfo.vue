31819d2d27528f3f5525b7b09438c3efaf7add9b
Juanma <juanmartin.pilon@gmail.com>
2024-03-06 14:33:42 -0300

    contacts can be modified correctly, now trying to work in some minor fixes, and also will validate fields of the update form with vee

--- frontend-vue/components/editContactInfo.vue
+++ frontend-vue/components/editContactInfo.vue
@@ -1,8 +1,41 @@
 <template>
-  </template>
+  <div>
+    <form @submit.prevent="saveChanges">
+      <div class="contactInfoCard grid grid-cols-2 gap-3">
+        <div class="contactBox">
+          <label for="name">Name:</label>
+          <input v-model="editedContact.name" id="name" />
+        </div>
+        <div class="contactBox ml-auto">
+          <label for="address">Address:</label>
+          <input v-model="editedContact.adress" id="adress" />
+        </div>
+        <div class="contactBox">
+          <label for="position">Title:</label>
+          <input v-model="editedContact.position" id="position" />
+        </div>
+        <div class="contactBox ml-auto">
+          <label for="phone">Phone:</label>
+          <input v-model="editedContact.number" id="phone" />
+        </div>
+        <div class="contactBox">
+          <label for="image">Profile Picture:</label>
+          <input v-model="editedContact.image" id="image" />
+        </div>
+        <div class="contactBox ml-auto">
+          <label for="email">Email:</label>
+          <input v-model="editedContact.email" id="email" />
+        </div>
+      </div>
+      <button type="submit">Save</button>
+    </form>
+  </div>
+</template>
   
   <script>
+  import { useContactStore,useAuthStore } from '#imports';
   import { ref } from 'vue';
+  import axios from 'axios';
   
   export default {
     props: {
@@ -11,13 +44,12 @@
         required: true,
       },
     },
-    setup(props) {
+    setup(props, { emit }) {
       const editedContact = ref({ ...props.contact });
-  
-      const saveChanges = () => {
 
+      const saveChanges = () => {
+        emit('saveChanges', editedContact.value);
       };
-  
       return {
         editedContact,
         saveChanges,
--- frontend-vue/pages/contactProfile.vue
+++ frontend-vue/pages/contactProfile.vue
@@ -1,53 +1,50 @@
 <template>
-  <button
-  class="backButton backButtonMd"
-  @click="goBack">
-<i class="fas fa-arrow-left fa-lg"></i>
-  <span class="contactSub">Back</span>
-</button>
-<div class="relative">
-<div class="grayBgSqr grayBgSqrMd">
-  <button v-if="!editing" class="editButtonMd" @click="editingContact">
-    EDIT
-  </button>
-  <button v-if="editing" class="editButtonMd" @click="saveChanges">
-    SAVE
-  </button>
-  <img
-    v-if="!loadingContactDetails && contact.image && !imageError"
-    :src="contact.image"
-    alt="profileImage"
-    class="profileImage profileImageMd"
-    @load="handleImageLoad"
-    @error="handleImageError"
-  />
-  <img
-    v-if="!loadingContactDetails && (!contact.image || imageError)"
-    src="../static/emptyContact.png"
-    alt="emptyProfileImage"
-    class="profileImage profileImageMd"
-  />
-</div>
-<div class="profileHeader profileHeaderMd">
-  <h1 class="inputText">{{ contact.name }}</h1>
-  <p class="labelForm mb-5">{{ contact.position }}</p>
-</div>
-<div class="contactInfoContainer mx-20">
-  <contactInfo v-if="!loadingContactDetails && contact" :contact="contact" />
-  <editContactInfo v-if="editing" :contact="contact" @saveChanges="saveChanges"/>
-</div>
-</div>
-<button
-  v-if="!editing"
-  class="editButton"
-  @click="editingContact"
->
-  EDIT
-</button>
+  <div>
+    <button class="backButton backButtonMd" @click="goBack">
+      <i class="fas fa-arrow-left fa-lg"></i>
+      <span class="contactSub">Back</span>
+    </button>
+    <div class="relative">
+      <div class="grayBgSqr grayBgSqrMd">
+        <button v-if="!editing" class="editButtonMd" @click="editingContact">
+          EDIT
+        </button>
+        <button v-if="editing" class="editButtonMd" @click="saveChanges">
+          SAVE
+        </button>
+        <img
+          v-if="!loadingContactDetails && contact.image && !imageError"
+          :src="contact.image"
+          alt="profileImage"
+          class="profileImage profileImageMd"
+          @load="handleImageLoad"
+          @error="handleImageError"
+        />
+        <img
+          v-if="!loadingContactDetails && (!contact.image || imageError)"
+          src="../static/emptyContact.png"
+          alt="emptyProfileImage"
+          class="profileImage profileImageMd"
+        />
+      </div>
+      <div class="profileHeader profileHeaderMd">
+        <h1 class="inputText">{{ contact.name }}</h1>
+        <p class="labelForm mb-5">{{ contact.position }}</p>
+      </div>
+      <div class="contactInfoContainer mx-20">
+        <contactInfo v-if="!editing" :contact="contact" />
+        <editContactInfo v-if="editing" :contact="contact" @saveChanges="saveChanges" />
+      </div>
+    </div>
+    <button v-if="!editing" class="editButton" @click="editingContact">
+      EDIT
+    </button>
+  </div>
 </template>
 
 <script>
-import editContactInfo from '#components'
+import contactInfo from '#components';
+import editContactInfo from '#components';
 import { useAuthStore, useContactStore } from '#imports';
 import axios from 'axios';
 
@@ -89,17 +86,25 @@ async fetchContacts(selectedContactId) {
 editingContact() {
   this.editing = true;
 },
-saveChanges() {
-  axios.put(`http://localhost:8000/api/contact/${this.contact.id}`, updatedContact)
-    .then(() => {
-      console.log('Contact updated successfully');
-      this.editing = false;
-    })
-    .catch((error) => {
+saveChanges(updatedContact) {
+  axios.put(`http://localhost:8000/api/contact/${this.contact.id}/edit`, updatedContact, {
+  headers: {
+    Authorization: `Bearer ${useAuthStore().token}`
+  }
+})
+  .then(() => {
+    console.log('Contact updated successfully');
+    this.editing = false;
+    
+  })
+  .catch((error) => {
+    if (error.response.status === 401) {
+      console.error('Unauthorized. Redirecting to login page.');
+    } else {
       console.error('Error updating contact:', error);
-    });
-  this.editing = false;
-},
+    }
+  });
+    },
 handleImageError() {
   this.imageError = true;
 },
@@ -108,9 +113,8 @@ goBack() {
 },
 },
 mounted() {
-const selectedContactId = useContactStore().getSelectedContactId() || 0;
-console.log('Selected Contact ID:', selectedContactId);
-this.fetchContacts(selectedContactId);
+  const selectedContactId = useContactStore().getSelectedContactId() || 0;
+  this.fetchContacts(selectedContactId);
 },
 };
 </script>
