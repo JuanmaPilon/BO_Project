00092c4bc6518f21ac56b14876689ee0903c81b0
Juanma <juanmartin.pilon@gmail.com>
2024-03-07 11:22:58 -0300

    Changes to the grid and tailwind.css

--- frontend-vue/pages/contact.vue
+++ frontend-vue/pages/contact.vue
@@ -98,4 +98,27 @@ export default {
 </script>
 
 <style scoped>
+@media (max-width: 767px) {
+  .grid-cols-1 {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1023px) {
+  .grid-cols-1 {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+}
+
+@media (min-width: 1024px) and (max-width: 1279px) {
+  .grid-cols-1 {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+}
+
+@media (min-width: 1280px) {
+  .grid-cols-1 {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+}
 </style>
